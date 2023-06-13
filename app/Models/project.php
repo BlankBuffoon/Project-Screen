54ab4ae4b30470ea6dc72ebf3bb8de841559ac68
blankbuffoon <aterepin@yandex.ru>
2023-06-13 17:15:03 +0500

    Migrations and models

---
+++ app/Models/project.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Project extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'status',
+        'maintainers',
+        'developers',
+    ];
+
+    use HasFactory;
+    use SoftDeletes;
+
+    public function maintainers() {
+        return $this->belongsToMany(User::class, 'maintainers');
+    }
+
+    public function developers() {
+        return $this->belongsToMany(User::class, 'developers');
+    }
+}
--- database/migrations/2023_06_13_105634_create_projects_table.php
+++ database/migrations/2023_06_13_105634_create_projects_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('projects', function (Blueprint $table) {
             $table->id();
+
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('description');
+            $table->string('status');
 
             $table->timestamps();
             $table->softDeletes();
@@ -29,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('projects');
     }
 };
