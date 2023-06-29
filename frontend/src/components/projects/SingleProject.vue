fe28ae9ab7a5e8284d1dbfa5102f86a03bae0541
Alex Terepin <aterepin@yandex.ru>
2023-06-30 03:24:25 +0500

    Changes in projects cards components

--- frontend/src/components/projects/SingleProject.vue
+++ frontend/src/components/projects/SingleProject.vue
@@ -1,4 +1,6 @@
 <script setup>
+import StatusCircle from './StatusCircle.vue';
+
 defineProps({
     project: {
         type: Object,
@@ -8,42 +10,9 @@ defineProps({
 
 <template>
     <div class="item">
-        <div class="left">
+        <div class="title">
+            <StatusCircle :status="project.status"/>
             <h2>{{ project.name }}</h2>
-            <p class="descr">{{ project.description }}</p>
-            <div class="meta">
-                <div class="meta-item">
-                    <p class="bullet">Status: </p>
-                    <span>{{ project.status }}</span>
-                </div>
-
-                <div class="meta-item">
-                    <p class="bullet">Maintainers:</p>
-                    <ul>
-                        <!-- <li>{{$maintainer->name}}</li> -->
-                        <li>Test</li>
-                        <li>Test</li>
-                        <li>Test</li>
-                    </ul>
-                </div>
-
-                <div class="meta-item">
-                    <p class="bullet">Developers:</p>
-                    <ul>
-                        <!-- <li>{{$developer->name}}</li> -->
-                        <li>Test</li>
-                        <li>Test</li>
-                        <li>Test</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="right">
-            <div class="item-expiration">
-                <p>/</p>
-            </div>
-            <p>Expiration date:</p>
-            <p>{{ project.expiration_date }}</p>
         </div>
     </div>
 </template>
@@ -53,84 +22,23 @@ defineProps({
 .item {
     min-height: 100%;
     min-width: 100%;
-    border-radius: 20px;
-    border: 2px solid rgb(102, 102, 102);
-    padding: 24px;
+    border-radius: 15px;
+    border: 3px solid #4E4E4E;
+    padding: 30px;
 
     display: flex;
     gap: 50px;
     align-items: center;
     justify-content: space-between;
+}
+
+.title {
+    display: flex;
+    gap: 14px;
+    align-items: center;
 
     h2 {
-        margin-bottom: 8px;
-    }
-
-    .bullet {
-        color: rgb(110, 110, 110);
-        padding: 4px 8px;
-        border-radius: 20px;
-        border: 2px solid rgb(110, 110, 110);
-        margin-right: 20px;
-        height: fit-content;
-        width: fit-content;
-    }
-
-    .descr {
-        margin-bottom: 18px;
-    }
-}
-
-.meta {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-start;
-    gap: 20px;
-
-    &-item {
-        display: grid;
-        grid-template-columns: 150px 200px;
-        align-items: center;
-
-        &:not(:last-child) {
-            padding-bottom: 20px;
-            border-bottom: 2px dashed rgb(169, 169, 169);
-        }
-    }
-
-    ul {
-        list-style-type: none;
-        display: flex;
-        flex-direction: column;
-
-        li:not(:last-child) {
-            margin-bottom: 6px;
-        }
-    }
-}
-
-
-.right {
-    .item-expiration {
-        width: 150px;
-        height: 150px;
-        border-radius: 15px;
-        border: 2px solid rgb(140, 140, 140);
-
-        display: flex;
-        justify-content: center;
-        align-items: center;
-
-        margin-bottom: 8px;
-
-        p {
-            font-size: 80px;
-        }
-    }
-
-    p {
-        text-align: center;
+        width: 100%;
     }
 }
 
---
+++ frontend/src/components/projects/StatusCircle.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+defineProps({
+    status: {
+        type: Number,
+    },
+})
+</script>
+
+<template>
+    <div v-if="status == 0" class="circle red"></div>
+    <div v-else-if="status == 1" class="circle yellow"></div>
+    <div v-else-if="status == 2" class="circle green"></div>
+    <div v-else-if="status == 3" class="circle white"></div>
+</template>
+
+<style lang="scss" scoped>
+.circle {
+    min-width: 20px;
+    min-height: 20px;
+    border-radius: 20px;
+
+    &.red {
+        background: var(--status-red-c);
+    }
+    &.yellow {
+        background: var(--status-yellow-c);
+    }
+    &.green {
+        background: var(--status-green-c);
+    }
+    &.white {
+        background: var(--status-white-c);
+    }
+}
+</style>
