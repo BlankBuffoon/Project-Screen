85778979f2ad18d70d20b750400de95a687bbc9e
blankbuffoon <aterepin@yandex.ru>
2023-07-06 17:05:12 +0500

    Update Api Axios service

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const apiService = axios.create({
   baseURL: 'http://localhost:8081/api', // Укажите базовый URL вашего API Laravel
+  withCredentials: true,
 });
 
 export default apiService;
